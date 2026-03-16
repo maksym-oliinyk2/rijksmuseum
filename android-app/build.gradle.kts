999ec0a6c9adc126f48ea15e7b63100b65377068
Maksym Oliinyk <maksym-oliinyk2@epam.com>
2026-03-16 22:51:16 +0200

    refine dependencies & move composables to library

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -33,6 +33,8 @@ configureTests()
 
 dependencies {
     implementation(project(":library"))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material)
     implementation(libs.compose.activity)
     implementation(libs.splashscreen)
 
