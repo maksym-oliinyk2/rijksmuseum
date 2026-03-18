520cec605fdd96cb684a3ff34f4d8e1b68135be3
maksym-oliinyk2 <58474371+maksym-oliinyk2@users.noreply.github.com>
2026-03-18 11:02:33 +0200

    Initial setup (#1)

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.kotlin.compose)
 }
 
 group = "io.github.oliinyk.maksym.rijksmuseum"
-version = "1.0.0"
+version = libraryVersion.toVersionName()
 
 kotlin {
     explicitApi()
@@ -27,13 +28,49 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
+                api(libs.coroutines.core)
+                api(libs.compose.ui)
+                api(libs.compose.runtime)
+                api(libs.compose.foundation)
+                api(libs.compose.components.ui.tooling.preview)
+                implementation(libs.compose.components.resources)
+                implementation(libs.bundles.coil)
+                implementation(libs.compose.material)
+                implementation(libs.compose.material.icons.extended)
+                implementation(libs.stdlib)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.logging)
+                implementation(libs.ktor.client.negotiation)
+                implementation(libs.ktor.serialization.json)
+                implementation(libs.serialization.core)
+                implementation(libs.ui.tooling.preview)
             }
         }
 
         commonTest {
             dependencies {
                 implementation(libs.kotlin.test)
+                implementation(libs.kotlin.test.annotations)
+            }
+        }
+
+        androidMain {
+            dependencies {
+                implementation(libs.ktor.client.cio)
+                implementation(libs.ktor.client.logging)
+                implementation(libs.coroutines.android)
+                implementation(libs.ktor.client.cio)
+            }
+        }
+
+        iosMain {
+            dependencies {
+                implementation(libs.ktor.client.ios)
             }
         }
     }
+}
+
+dependencies {
+    androidRuntimeClasspath(libs.ui.tooling)
 }
---
+++ library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package io.github.oliinyk.maksym.rijksmuseum.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColors(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    primaryVariant = Pink80
+)
+
+private val LightColorScheme = lightColors(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    primaryVariant = Pink40
+)
+
+@Composable
+public fun RijksmuseumTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val colors = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        content = content
+    )
+}
