c837aead5cdc03b682acbbc3acbb119e035382e5
Maksym Oliinyk <maksym-oliinyk2@epam.com>
2026-03-18 14:22:06 +0200

    remove unused fields from dtos

--- rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/artworks/details/VisualItemDetails.kt
+++ rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/artworks/details/VisualItemDetails.kt
@@ -5,10 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 public data class VisualItemDetails(
-    @SerialName("@context")
-    val context: String,
     val id: String,
-    val type: String,// expected VisualItem
     @SerialName("digitally_shown_by")
     val digitallyShownBy: List<DigitalObject> = emptyList()
 )
@@ -16,13 +13,10 @@ public data class VisualItemDetails(
 @Serializable
 public data class DigitalObject(
     val id: String,
-    val type: String,//expected DigitalObject
 )
 
 @Serializable
 public data class DigitalObjectDetails(
-    val id: String,
-    val type: String,//expected DigitalObject
     @SerialName("access_point")
     val accessPoint: List<AccessPoint> = emptyList()
 )
@@ -30,5 +24,4 @@ public data class DigitalObjectDetails(
 @Serializable
 public data class AccessPoint(
     val id: String,
-    val type: String,//expected DigitalObject
 )
