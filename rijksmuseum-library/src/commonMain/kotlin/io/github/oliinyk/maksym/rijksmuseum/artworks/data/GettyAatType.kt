cafcd6fc2e510f4da188063c740f5bc4ced4ee96
Maksym Oliinyk <maksym-oliinyk2@epam.com>
2026-03-20 19:33:12 +0200

    Add display names to GettyAatType.kt

--- rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/artworks/data/GettyAatType.kt
+++ rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/artworks/data/GettyAatType.kt
@@ -2,6 +2,23 @@ package io.github.oliinyk.maksym.rijksmuseum.artworks.data
 
 import io.github.oliinyk.maksym.rijksmuseum.domain.Url
 import io.github.oliinyk.maksym.rijksmuseum.domain.UrlFrom
+import io.github.oliinyk.maksym.rijksmuseum.res.Res
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_brief_text
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_collection
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_description
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_dimensions
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_documentation
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_dutch
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_english
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_inscription
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_mentioned
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_notes
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_object_number
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_original_series_title
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_original_title
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_production
+import io.github.oliinyk.maksym.rijksmuseum.res.getty_aat_type_work_type
+import org.jetbrains.compose.resources.StringResource
 
 /**
  * Common Getty Art & Architecture Thesaurus (AAT) types used by the Rijksmuseum API
@@ -9,71 +26,82 @@ import io.github.oliinyk.maksym.rijksmuseum.domain.UrlFrom
  */
 public enum class GettyAatType(
     internal val id: Url,
+    public val displayName: StringResource,
 ) {
     /**
      * Inscriptions or marks ("Opschriften / Merken")
      */
-    Inscription(UrlFrom("http://vocab.getty.edu/aat/300435414")),
+    Inscription(UrlFrom("http://vocab.getty.edu/aat/300435414"), Res.string.getty_aat_type_inscription),
 
     /**
      * Descriptive notes ("Beschrijving")
      */
-    Notes(UrlFrom("http://vocab.getty.edu/aat/300435452")),
+    Notes(UrlFrom("http://vocab.getty.edu/aat/300435452"), Res.string.getty_aat_type_notes),
 
     /**
      * General description ("Beschrijving" - often used with Notes)
      */
-    Description(UrlFrom("http://vocab.getty.edu/aat/300404670")),
+    Description(UrlFrom("http://vocab.getty.edu/aat/300404670"), Res.string.getty_aat_type_description),
 
     /**
      * Documentation or references ("Documentatie")
      */
-    Documentation(UrlFrom("http://vocab.getty.edu/aat/300435416")),
+    Documentation(UrlFrom("http://vocab.getty.edu/aat/300435416"), Res.string.getty_aat_type_documentation),
 
     /**
      * Dimensions ("Afmetingen")
      */
-    Dimensions(UrlFrom("http://vocab.getty.edu/aat/300435430")),
+    Dimensions(UrlFrom("http://vocab.getty.edu/aat/300435430"), Res.string.getty_aat_type_dimensions),
 
     /**
      * Original title on object ("originele titel op object")
      */
-    OriginalTitle(UrlFrom("https://id.rijksmuseum.nl/22015530")),
+    OriginalTitle(UrlFrom("https://id.rijksmuseum.nl/22015530"), Res.string.getty_aat_type_original_title),
 
     /**
      * Original series title ("originele serietitel")
      */
-    OriginalSeriesTitle(UrlFrom("https://id.rijksmuseum.nl/22015532")),
+    OriginalSeriesTitle(UrlFrom("https://id.rijksmuseum.nl/22015532"), Res.string.getty_aat_type_original_series_title),
+
+    /**
+     * English language
+     */
+    English(UrlFrom("http://vocab.getty.edu/aat/300388277"), Res.string.getty_aat_type_english),
+
+    /**
+     * Dutch language
+     */
+    Dutch(UrlFrom("http://vocab.getty.edu/aat/300388256"), Res.string.getty_aat_type_dutch),
 
     /**
      * Collection or provenance information ("Collectie")
      */
-    Collection(UrlFrom("http://vocab.getty.edu/aat/300026687")),
+    Collection(UrlFrom("http://vocab.getty.edu/aat/300026687"), Res.string.getty_aat_type_collection),
 
     /**
      * Brief text classification
      */
-    BriefText(UrlFrom("http://vocab.getty.edu/aat/300418049")),
+    BriefText(UrlFrom("http://vocab.getty.edu/aat/300418049"), Res.string.getty_aat_type_brief_text),
 
     /**
      * Type of Work classification
      */
-    WorkType(UrlFrom("http://vocab.getty.edu/aat/300435443")),
+    WorkType(UrlFrom("http://vocab.getty.edu/aat/300435443"), Res.string.getty_aat_type_work_type),
 
     /**
      * Object number classification
      */
-    ObjectNumber(UrlFrom("http://vocab.getty.edu/aat/300312355")),
+    ObjectNumber(UrlFrom("http://vocab.getty.edu/aat/300312355"), Res.string.getty_aat_type_object_number),
 
     /**
      * Production classification
      */
-    Production(UrlFrom("http://vocab.getty.edu/aat/300404450")),
+    Production(UrlFrom("http://vocab.getty.edu/aat/300404450"), Res.string.getty_aat_type_production),
 
     /**
      * Mentioned in documentation or reference
      */
-    Mentioned(UrlFrom("http://vocab.getty.edu/aat/300028705"));
+    Mentioned(UrlFrom("http://vocab.getty.edu/aat/300028705"), Res.string.getty_aat_type_mentioned);
 
     internal companion object {
         fun fromId(id: Url): GettyAatType? = entries.firstOrNull { it.id == id }
