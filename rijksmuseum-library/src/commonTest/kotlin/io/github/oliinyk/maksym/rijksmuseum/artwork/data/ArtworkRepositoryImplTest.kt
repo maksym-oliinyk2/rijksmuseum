32c748891777aad63eea97df05a6cae5bc372c87
Maksym Oliinyk <maksym-oliinyk2@epam.com>
2026-03-21 18:54:49 +0200

    add tests

---
+++ rijksmuseum-library/src/commonTest/kotlin/io/github/oliinyk/maksym/rijksmuseum/artwork/data/ArtworkRepositoryImplTest.kt
@@ -0,0 +1,48 @@
+package io.github.oliinyk.maksym.rijksmuseum.artwork.data
+
+import arrow.core.right
+import io.github.oliinyk.maksym.rijksmuseum.artwork.domain.Artwork
+import io.github.oliinyk.maksym.rijksmuseum.artwork.domain.Title
+import io.github.oliinyk.maksym.rijksmuseum.artworks.list.TestRijksmuseumApi
+import io.github.oliinyk.maksym.rijksmuseum.domain.UrlFrom
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ArtworkRepositoryImplTest {
+
+    private val testUrl = UrlFrom("https://example.com/1")
+    private val testArtwork = Artwork(
+        url = testUrl,
+        title = Title("Artwork 1"),
+        primaryImage = UrlFrom("https://example.com/1.jpg"),
+        descriptions = listOf()
+    )
+
+    @Test
+    fun `when fetchArtworkDetails and cache is empty then it fetches from api`() = runTest {
+        val api = TestRijksmuseumApi(
+            artworksDetails = mapOf(testUrl to testArtwork.right())
+        )
+        val cache = ValueHolder<Artwork>()
+        val repository = ArtworkRepositoryImpl(api, cache)
+
+        val result = repository.fetchArtworkDetails(testUrl)
+
+        assertEquals(testArtwork.right(), result)
+    }
+
+    @Test
+    fun `when fetchArtworkDetails and cache hit then it returns cached data`() = runTest {
+        val api = TestRijksmuseumApi(
+            artworksDetails = emptyMap()
+        )
+        val cache = ValueHolder(testArtwork)
+        val repository = ArtworkRepositoryImpl(api, cache)
+
+        val result = repository.fetchArtworkDetails(testUrl)
+
+        assertEquals(testArtwork.right(), result)
+        assertEquals(null, cache.value)
+    }
+}
