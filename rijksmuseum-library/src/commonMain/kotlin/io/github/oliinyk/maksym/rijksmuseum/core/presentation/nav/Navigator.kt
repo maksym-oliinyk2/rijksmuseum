8353e46ac97194dca7c5bb30b0ae84e52d735a56
Maksym Oliinyk <maksym-oliinyk2@epam.com>
2026-03-23 00:29:52 +0200

    register serializator for Artwork

--- rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/core/presentation/nav/Navigator.kt
+++ rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/core/presentation/nav/Navigator.kt
@@ -1,8 +1,14 @@
 package io.github.oliinyk.maksym.rijksmuseum.core.presentation.nav
 
 import androidx.navigation3.runtime.NavKey
+import androidx.savedstate.serialization.SavedStateConfiguration
+import io.github.oliinyk.maksym.rijksmuseum.core.data.dto.ArtworkSerializer
 import io.github.oliinyk.maksym.rijksmuseum.core.domain.Artwork
 import io.github.oliinyk.maksym.rijksmuseum.feature.artworkdetails.presentation.ArtworkDetailsDestination
+import io.github.oliinyk.maksym.rijksmuseum.feature.artworkdetails.presentation.registerArtworkNavEntry
+import io.github.oliinyk.maksym.rijksmuseum.feature.artworks.presentation.registerArtworksNavEntry
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
 
 internal class Navigator(
     private val navBackStack: MutableList<NavKey>,
@@ -14,5 +20,15 @@ internal class Navigator(
 
     fun navigateToDetails(artwork: Artwork) {
         navBackStack.add(ArtworkDetailsDestination(artwork))
+    }
+}
+
+internal val NavSavedStateConfig = SavedStateConfiguration {
+    serializersModule = SerializersModule {
+        contextual(Artwork::class, ArtworkSerializer)
+        polymorphic(NavKey::class) {
+            registerArtworksNavEntry()
+            registerArtworkNavEntry()
+        }
     }
 }
