93d003d001405f04e5cca70c5e3da100bf912d9f
Maksym Oliinyk <maksym-oliinyk2@epam.com>
2026-03-22 14:37:47 +0200

    simplify domain a bit

--- rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/artwork/domain/Artwork.kt
+++ rijksmuseum-library/src/commonMain/kotlin/io/github/oliinyk/maksym/rijksmuseum/artwork/domain/Artwork.kt
@@ -5,6 +5,9 @@ import io.github.oliinyk.maksym.rijksmuseum.artworks.data.GettyAatType
 import io.github.oliinyk.maksym.rijksmuseum.domain.Url
 import kotlin.jvm.JvmInline
 
+public typealias Description = NonEmptyString
+public typealias Title = NonEmptyString
+
 public data class Artwork(
     val url: Url,
     val title: Title,
@@ -12,28 +15,16 @@ public data class Artwork(
     val linguisticObjects: List<LinguisticObject>,
 )
 
+@JvmInline
+public value class NonEmptyString(
+    public val value: String
+) : CharSequence by value {
+    init {
+        require(value.isNotBlank()) { "Cannot be blank" }
+    }
+}
+
 public data class LinguisticObject(
     val type: GettyAatType,
     val descriptions: NonEmptyList<Description>,
 )
-
-@JvmInline
-public value class Description(
-    public val value: String
-) {
-    init {
-        require(value.isNotBlank()) { "Description cannot be blank" }
-    }
-}
-
-/**
- * Represents the title of an artwork.
- */
-@JvmInline
-public value class Title(
-    public val value: String
-) {
-    init {
-        require(value.isNotBlank()) { "Title cannot be blank" }
-    }
-}
